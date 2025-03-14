851bfbecd32991c65867048582ee835463469871
calebsg225 <calebsg2253284@gmail.com>
2025-03-13 23:13:51 -0400

    make initial integration test
    - set up library in Cargo.toml
    - create tests dir
    - move code around to support lib structure
    - create basic test

---
+++ tests/health_check.rs
@@ -0,0 +1,23 @@
+//! tests/health_checks.rs
+
+use zero2prod::run;
+
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Health check failed");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+}
