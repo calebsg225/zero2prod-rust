97bfc77b4912ba567541521979d34f6710335a1b
calebsg225 <calebsg2253284@gmail.com>
2025-03-15 00:13:27 -0400

    name the docker container in init_db.sh
    - define a container_name
    - if a docker with name exists, delete it
    - run new container with name
    
    **might not be such a good idea, idk yet, thought id try it...

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -14,12 +14,20 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 # check if a custom host has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
+# docker container name
+CONTAINER_NAME="z2p_db"
+
+# remove previous container if it exists
+docker rm ${CONTAINER_NAME} || true
+
 # launch postgress using Docker
 docker run \
+	--name ${CONTAINER_NAME} \
 	-e POSTGRES_USER=${DB_USER} \
 	-e POSTGRES_PASSWORD=${DB_PASSWORD} \
 	-e POSTGRES_DB=${DB_NAME} \
 	-p "${DB_PORT}":5432 \
 	-d postgres \
-	postgres -N 1000
+	postgres -N 1000 \
 	# ^ increased maximum number of connections for testing purposes
+
