6911f978db6933876f663139d884c1cd0269e8bb
calebsg225 <calebsg2253284@gmail.com>
2025-03-14 21:05:53 -0400

    use random unused ports with TcpListener

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,23 +1,32 @@
 //! tests/health_checks.rs
 
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
-
+    // Arrange
+    let address = spawn_app();
     let client = reqwest::Client::new();
+
+    // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Health check failed");
 
+    // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
