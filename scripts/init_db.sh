37cef5bf5ce52d63ec851a9b3067848a0021d522
calebsg225 <calebsg2253284@gmail.com>
2025-03-15 21:12:05 -0400

    update init_db.sh
    DEAL WITH SUDO ISSUE:
    issue:
    - docker requires sudo (unless I change that on my machine)
    - sqlx won't work with sudo (unless I change that on my machine)
    
    solution:
    - don't run the entire script with sudo: instead add sudo before each
      docker command in the script
    
    CHECK THAT PACKAGES ARE INSTALLED:
    have if statemets at the beginning of the script to make sure 'sqlx'
    and 'psql' are both installed

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,18 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then
+	echo >&2 "Error: psql is not installed"
+	exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+	echo >&2 "Error: sqlx is not installed"
+	echo >&2 "Use:"
+	echo >&2 "	cargo install --version='~0.7' sqlx-cli --no-default-features --features rustls, postgres"
+	echo >&2 "to install it."
+	exit 1
+fi
 
 # check if a custom user has been set, otherwise default to 'postgres'
 DB_USER="${POSTGRES_USER:=postgres}"
@@ -18,16 +30,16 @@ DB_HOST="${POSTGRES_HOST:=localhost}"
 CONTAINER_NAME="${POSTGRES_DOCKER_CONTAINER_NAME:=z2pdb}"
 
 # if the docker container is running, kill it
-(docker ps | grep ${CONTAINER_NAME} && docker kill ${CONTAINER_NAME}) || true
+(sudo docker ps | grep ${CONTAINER_NAME} && sudo docker kill ${CONTAINER_NAME}) || true
 
 # remove previous container if it exists
-docker rm ${CONTAINER_NAME} || true
+sudo docker rm ${CONTAINER_NAME} || true
 
 # test grep
-docker ps -a | grep foobar || true
+sudo docker ps -a | grep foobar || true
 
 # launch postgress using Docker
-docker run \
+sudo docker run \
 	--name ${CONTAINER_NAME} \
 	-e POSTGRES_USER=${DB_USER} \
 	-e POSTGRES_PASSWORD=${DB_PASSWORD} \
@@ -36,4 +48,16 @@ docker run \
 	-d postgres \
 	postgres -N 1000 \
 	# ^ increased maximum number of connections for testing purposes
+
+export PGPASSWORD="${DB_PASSWORD}"
+until pg_isready -h ${DB_HOST} -U ${DB_USER} -p ${DB_PORT} -d "postgres"; do
+	echo >&2 "Postgres is still unavailable - sleeping"
+	sleep 1
+done
+
+echo >&2 "Postgres is up and running on port ${DB_PORT}"
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+cargo sqlx database create
 
